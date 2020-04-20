e3c87ee1f626ecc817b944ee5e0aa0d107287bb6
Nelson Roza Junior <jr.1403@hotmail.com>
2020-04-19 21:26:23 -0300

    Identity context and migrations

--- API/Repository/Context/EventContext.cs
+++ API/Repository/Context/EventContext.cs
@@ -1,9 +1,12 @@
-﻿using Angular.API.Domain.Models;
+﻿using Angular.API.Domain.Identity;
+using Angular.API.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Angular.API.Repository.Context
 {
-    public class EventContext : DbContext
+    public class EventContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public EventContext(DbContextOptions<EventContext> options) : base(options)
         {
@@ -21,10 +24,25 @@ namespace Angular.API.Repository.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(ur => ur.UserRoles)
+                    .HasForeignKey(k => k.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(ur => ur.UserRoles)
+                    .HasForeignKey(k => k.UserId)
+                    .IsRequired();
+            });
+
             modelBuilder.Entity<SpeakerEvent>()
                 .HasKey(p => new { p.EventId, p.SpeakerId });
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- API/Repository/Migrations/20200420001753_Identity.Designer.cs
+++ API/Repository/Migrations/20200420001753_Identity.Designer.cs
@@ -4,14 +4,16 @@ using Angular.API.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Angular.API.Repository.Migrations
 {
     [DbContext(typeof(EventContext))]
-    partial class EventContextModelSnapshot : ModelSnapshot
+    [Migration("20200420001753_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
